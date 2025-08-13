25d3b76095fcab3e1f819c5f80371e749c9ec749
DEVctr <vferreira487@gmail.com>
2025-08-12 23:13:38 -0300

    ✨  feat: add ENUMs for Task Status and Task Priority

---
+++ src/main/java/com/todo/todolist/entities/enums/TaskPriority.java
@@ -0,0 +1,8 @@
+package com.todo.todolist.entities.enums;
+
+public enum TaskPriority {
+    LOW,
+    MEDIUM,
+    HIGH,
+    URGENT
+}
---
+++ src/main/java/com/todo/todolist/entities/enums/TaskStatus.java
@@ -0,0 +1,7 @@
+package com.todo.todolist.entities.enums;
+
+public enum TaskStatus {
+    COMPLETED,
+    PENDING,
+    PROGRESS
+}
